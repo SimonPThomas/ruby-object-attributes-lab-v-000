1796a59b54957d8942b30d3080511ca6eddcba21
Simon Thomas <simonpthomas89@gmail.com>
2018-09-24 17:05:37 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,10 +1,10 @@
 class Dog
-  def initialize (name)
-    @name = name
-  end
-  def name
-    @name
-  end
+  # def initialize (name)
+  #   @name = name
+  # end
+  # def name
+  #   @name
+  # end
 end
 
 Fido = Dog.new
